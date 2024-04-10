daf2f8c53f7c152e123f7705dd089d55bcbe7aaa
L-Hidalgo <l0hidalgo001698@gmail.com>
2024-04-09 23:19:54 -0400

    migracion de datos

---
+++ app/Models/Departamento.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Departamento extends Model
+{
+    use HasFactory;
+
+    protected $table = 'dde_departamentos';
+
+    protected $primaryKey = 'id_departamento';
+
+    protected $fillable = [
+        'nombre_departamento',
+        'gerencia_id'
+    ];
+
+    protected $casts = [
+        'fecha_inicio' => 'datetime',
+        'fecha_fin' => 'datetime',
+    ];
+
+    public function puesto()
+    {
+        return $this->hasMany(Puesto::class);
+    }
+
+    public function Gerencia() {
+        return $this->belongsTo(Gerencia::class);
+    }
+
+}
+
+
---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImportarExcelController;
+
+
+Route::post('/planilla', [ImportarExcelController::class, 'importExcel']); 
+
