55a077aeddb4cc1359c2bac01680e0becf0f9f4a
L-Hidalgo <l0hidalgo001698@gmail.com>
2024-04-10 01:16:07 -0400

    crear incorporaciones y personas servicios

---
+++ app/Http/Controllers/PersonasController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Persona;
+use Illuminate\Http\Request;
+class PersonasController extends Controller
+{
+
+  public function crearPersona(Request $request)
+  {
+    $validatedData = $request->validate([
+      'ci_persona' => 'string',
+      'primer_apellido_persona' => 'string',
+      'segundo_apellido_persona' => 'string',
+      'nombre_persona' => 'string',
+    ]);
+    // dd($validatedData);
+
+    $persona = Persona::create([]);
+    // agregar campos para actualizacion
+    $persona->ci_persona = $validatedData['ci_persona'];
+    $persona->primer_apellido_persona = $validatedData['primer_apellido_persona'];
+    $persona->segundo_apellido_persona = $validatedData['segundo_apellido_persona'];
+    $persona->nombre_persona = $validatedData['nombre_persona'];
+    // guardar
+    $persona->save();
+    return $this->sendSuccess($persona);
+  }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImportarExcelController;
-
+use App\Http\Controllers\IncorporacionesController;
+use App\Http\Controllers\PersonasController;
 
 Route::post('/planilla', [ImportarExcelController::class, 'importExcel']); 
 
+// Incorporaciones
+Route::put('/incorporacion',[IncorporacionesController::class, 'crearActualizarIncorporacion']);
+
+/* ------------------------------------------ Personas ------------------------------------------ */
+Route::post('/personas',[PersonasController::class, 'crearPersona']);
